b06d2674652db0c1d94617705cc3688711b1dc1b
suman-rashid <suman.rashid@phaedrasolutions.com>
2020-06-11 20:02:01 +0500

    Connection with Salesforce and Upload Data via Bulk API

--- businesslogic.js
+++ businesslogic.js
@@ -1,17 +1,22 @@
 var waitAsec = require('./waitAsec')
-
-/*
-
-This function is where all your work will go. 
-
-*/
+var salesforceconnectionlogin = require('./login')
 
 module.exports = async (params) => {
-    await waitAsec() //The only reason I put this in for the demo is because I know that in real life this function will be async and take some time
-    console.log(params)
-    //csv_url
-    //object_name 
-
+    await waitAsec() 
+    let connection = await salesforceconnectionlogin(); 
+    console.log("Access Token is:" + connection.accessToken)
+    var filename = "./CSVs/" + params.csv_url; 
+    var csvFileIn = require('fs').createReadStream(filename);
+    connection.bulk.load(params.object_name, "insert", csvFileIn, function(err, response) {
+        if (err) { return console.error(err); }
+        for (var i=0; i < response.length; i++) {
+            if (response[i].success) {
+                console.log("#" + (i+1) + `loaded successfully into ${params.object_name} object, id =`  + response[i].id);
+            } else {
+                console.log("#" + (i+1) +  `error occurred in ${params.object_name} object, message = ` + response[i].errors.join(', '));
+            }
+        }
+    });
     return {
         paramsReceived: params,
         time: new Date().toLocaleString()
